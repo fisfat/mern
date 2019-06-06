39806d572b98b367b7aa6ae7014506d8016a0871
fisfat <fatoyinbooluwafisayo@gmail.com>
2019-06-06 23:35:10 +0100

    rounding up

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,6 @@
 module.exports = {
-  mongoURI: 'mongodb://brad:brad@ds231725.mlab.com:31725/devconnector',
-  secretOrKey: 'secret'
+  mongoURI: "mongodb://127.0.0.1:27017/admin",
+  //   mongoURI:
+  // "mongodb+srv://fisfat:oluwafisayo1@devconnector-iphuw.mongodb.net/test?retryWrites=true",
+  secretOrKey: "secret"
 };
